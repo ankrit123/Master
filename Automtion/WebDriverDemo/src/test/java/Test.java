8ea8f6bfd0158ef928e3ec9728f7ba94a850aa07
ankrit <ankrit.tiwari20@gmail.com>
2023-06-27 01:05:09 +0530

    ankrit first commit

--- Automtion/WebDriverDemo/src/test/java/Test.java
+++ Automtion/WebDriverDemo/src/test/java/Test.java
@@ -3,11 +3,16 @@ public class Test {
 	public void doLogin() {
 		System.out.println("login Successful!!");
 	}
+	
+	public void doLogout() {
+		System.out.println("Logout Successful!!");
+	}
 
 	public static void main(String[] args) {
 		
 		Test test = new Test();
 		test.doLogin();
+		test.doLogout();
 	}
 
 }
